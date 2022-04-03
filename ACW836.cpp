a8dc91e3e8f968903d74e4c856950451ae2277ff
jackhexiangyu12 <22920202204559@stu.xmu.edu.cn>
2022-04-03 20:55:54 +0800

    4

--- ACW836.cpp
+++ ACW836.cpp
@@ -1,41 +1,42 @@
-
-#include "iostream"
-#include "vector"
-
-using namespace std;
-
-//vector<vector<int>> q;
-
-int p[100100];
-
-int main() {
-    int n, m;
-    char operation;
-    int a, b;
-    for(int i = 0; i < 100100; i++) {
-        p[i] = i;
-    }
-//    for(int i=0;i<m;i++){
-//        q[i].push_back(i);
+//#include "iostream"
+//using namespace std;
+//
+//int p[100100];
+//int main() {
+//    long long int n, m;
+//    char operation;
+//    long long int a, b;
+//    cin >> n >> m;
+//    for(int i = 0; i <=n; i++) {
+//        p[i] = i;
 //    }
-    cin >> n >> m;
-    for (int i = 0; i < m; i++) {
-        cin >> operation >> a >> b;
-        if (operation == 'M') {
-            p[a] = b;
-        }
-        if (operation == 'Q') {
-            while (p[a] != a) {
-                a = p[a];
-            }
-            while (p[b] != b) {
-                b = p[b];
-            }
-            if (a == b) {
-                cout << "Yes" << endl;
-            } else {
-                cout << "No" << endl;
-            }
-        }
-    }
-}
+//    for (int i = 0; i < m; i++) {
+//        cin >> operation >> a >> b;
+//        if (operation == 'M') {
+//            while(p[b] != b) {
+//                b = p[b];
+//            }
+//            int temp_a= a;
+//            a=p[a];
+//            while(p[a] != a) {
+//                temp_a=a;
+//                a = p[a];
+//                p[temp_a] = b;
+//            }
+//            p[a]=b;
+//        }
+//        if (operation == 'Q') {
+//            while (p[a] != a) {
+//                a = p[a];
+//            }
+//            while (p[b] != b) {
+//                b = p[b];
+//            }
+//            if (a == b) {
+//                cout << "Yes" << endl;
+//            } else {
+//                cout << "No" << endl;
+//            }
+//        }
+//    }
+//}
